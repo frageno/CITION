979a15c8d17a72556f5414ffea7330e98254081e
frageno <szymon.piatkowski27@gmail.com>
2022-01-18 22:27:45 +0100

    added automated convert sass into css

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-dart-sass');
+
+
+gulp.task('sass', function(){
+    return gulp.src('./assets/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./assets/css'))
+});
+
+gulp.task('watch', function(){
+    gulp.watch('./assets/scss/**/*.scss', gulp.series(['sass']));
+});
+
+
