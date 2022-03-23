ec981822691329c625dc7ecd2526f30036cea6f6
frageno <szymon.piatkowski27@gmail.com>
2022-03-23 20:47:30 +0100

    #2 Create pagination

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,21 @@ const gulp = require('gulp');
 const sass = require('gulp-dart-sass');
 const browserSync = require('browser-sync').create();
 
-
+// SASS TASK
 gulp.task('sass', function(){
-    return gulp.src('./assets/scss/**/*.scss')
+    return gulp.src('./assets/scss/**/*.scss', { sourcemaps: true })
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./assets/css'))
+        .pipe(gulp.dest('./assets/css', { sourcemaps: '.' }))
+        
 });
+
+
+
+gulp.task('browsersyncReload', function(cb){
+    browserSync.reload();
+    cb();
+});
+
 
 gulp.task('watch', function(){
     gulp.watch('./assets/scss/**/*.scss', gulp.series(['sass']));
