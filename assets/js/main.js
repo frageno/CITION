2516cf65ed7da3338e1ef62086b8746e6147c0b0
frageno <szymon.piatkowski27@gmail.com>
2022-03-31 20:45:39 +0200

    #4 Creating FAQ page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,1 @@
-let hamburgerBtn = document.querySelector('.c-nav__hamburger');
-
-hamburgerBtn.addEventListener('click', () => {
-    let navigation = document.querySelector('.c-nav__navigation');
-    let hamburgerLine = document.querySelector('.c-nav__hamburger');
-    navigation.classList.toggle('c-nav__active');
-    hamburgerLine.classList.toggle('c-nav__change');
-});
+let hamburgerBtn=document.querySelector(".c-nav__hamburger");hamburgerBtn.addEventListener("click",(()=>{let e=document.querySelector(".c-nav__navigation"),t=document.querySelector(".c-nav__hamburger");e.classList.toggle("c-nav__active"),t.classList.toggle("c-nav__change")}));
--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,99 @@
-'use strict';
+// 'use strict';
 
-const gulp = require('gulp');
+// const gulp = require('gulp');
+// const sass = require('gulp-dart-sass');
+// const browserSync = require('browser-sync').create();
+
+// // SASS TASK
+// gulp.task('sass', function(){
+//     return gulp.src('./assets/scss/**/*.scss', { sourcemaps: true })
+//         .pipe(sass().on('error', sass.logError))
+//         .pipe(gulp.dest('./assets/css', { sourcemaps: '.' }))
+        
+// });
+
+
+
+// gulp.task('browsersyncReload', function(cb){
+//     browserSync.reload();
+//     cb();
+// });
+
+
+// gulp.task('watch', function(){
+//     gulp.watch('./assets/scss/**/*.scss', gulp.series(['sass']));
+// });
+
+
+const {src, dest, watch, series} = require('gulp');
 const sass = require('gulp-dart-sass');
+const prefix = require('gulp-autoprefixer');
+const minify = require('gulp-clean-css');
+const terser = require('gulp-terser');
+const imagemin = require('gulp-imagemin');
 const browserSync = require('browser-sync').create();
 
-// SASS TASK
-gulp.task('sass', function(){
-    return gulp.src('./assets/scss/**/*.scss', { sourcemaps: true })
+// SCSS
+
+function compilescss(){
+    return src('./assets/scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./assets/css', { sourcemaps: '.' }))
-        
-});
+        .pipe(prefix())
+        .pipe(minify())
+        .pipe(dest('./assets/css'))
+}
 
+// JS
 
+function jsmin(){
+    return src('./assets/js/**/*.js')
+        .pipe(terser())
+        .pipe(dest('./assets/js'))
+}
 
-gulp.task('browsersyncReload', function(cb){
+// images
+
+function optimizeimg(){
+    return src('./assets/img/**/*.{jpg,png,svg,gif}')
+        .pipe(imagemin([
+            imagemin.mozjpeg({ quality:80, progressive:true }),
+            imagemin.optipng({ optimizationLevel:2 }),
+        ]))
+        .pipe(dest('/assets/img'))
+}
+
+// browserSync
+
+function browserSyncServe(cb){
+    browserSync.init({
+        server: {
+            baseDir: '.'
+        }
+    });
+    cb();
+}
+
+function browsersyncReload(cb){
     browserSync.reload();
     cb();
-});
+}
 
 
-gulp.task('watch', function(){
-    gulp.watch('./assets/scss/**/*.scss', gulp.series(['sass']));
-});
+// watch task
 
+function watchTask() {
+    watch('*.html', browsersyncReload)
+    watch('assets/scss/**/*.scss', series(compilescss, browsersyncReload));
+    watch('assets/js/**/*.js', series(jsmin, browsersyncReload));
+    watch('assets/img/**/*.{jpg,png,svg,gif}', optimizeimg);
+}
 
-
-
+exports.default = series(
+    compilescss,
+    jsmin,
+    optimizeimg,
+    browserSyncServe,
+    watchTask
+);
 
 
